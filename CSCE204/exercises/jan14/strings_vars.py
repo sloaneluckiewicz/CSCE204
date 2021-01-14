053022bc5956343609c7cace246a52d92c9ca31c
sloaneluckiewicz <77337170+sloaneluckiewicz@users.noreply.github.com>
2021-01-14 09:42:10 -0500

    Add files via upload

---
+++ CSCE204/exercises/jan14/strings_vars.py
@@ -0,0 +1,18 @@
+# Author: Sloane Luckiewicz
+# Learning Strings and Variables
+first_name = "Sloane"
+middle_name = "Elisabeth"
+last_name = "Luckiewicz"
+
+#print(first_name + " " + last_name)
+print(first_name + " " + middle_name + " " + last_name)
+
+# print(f"{first_name} {last_name}")
+print(f"Welcome {first_name} {last_name}")""
+
+print("first line\last line")
+
+print("""I
+Love
+multi
+lines""")

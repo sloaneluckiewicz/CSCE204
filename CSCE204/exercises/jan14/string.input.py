54b7cdb3de173e8a0ed08d1db93e0ee5bc325476
sloaneluckiewicz <77337170+sloaneluckiewicz@users.noreply.github.com>
2021-01-15 13:17:44 -0500

    Add files via upload

---
+++ CSCE204/exercises/jan14/string.input.py
@@ -0,0 +1,5 @@
+print("Welcome to my program")
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+print(f"Hi {first_name} {last_name}!")
---
+++ CSCE204/exercises/jan14/strings_vars.py
@@ -0,0 +1,17 @@
+# Author: Sloane Luckiewicz
+# Learning Strings and Variables
+first_name = "Sloane"
+middle_name = "Elisabeth"
+last_name = "Luckiewicz"
+
+#print(first_name + " " + last_name)
+print(first_name + " " + middle_name + " " + last_name)
+
+# print(f"{first_name} {last_name}")
+print(f"Welcome {first_name} {last_name}")
+print("first line\nlast line")
+
+print("""I
+Love
+multi
+lines""")

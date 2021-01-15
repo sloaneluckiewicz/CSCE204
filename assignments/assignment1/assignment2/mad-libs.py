a570339687002367643f2341c9e942b93fdc0df1
sloaneluckiewicz <77337170+sloaneluckiewicz@users.noreply.github.com>
2021-01-15 12:54:30 -0500

    Add files via upload

---
+++ assignments/assignment1/assignment2/mad-libs.py
@@ -0,0 +1,16 @@
+# Author: Sloane Luckiewicz
+
+college_name = "University of South Carolina"
+adjective1 = "sparkly"
+body_part = "pinky finger"
+an_object = "lamp"
+smell = "lavendar"
+action = "spin"
+adjective2 = "clever"
+
+print("Let's begin shall we?")
+print("It was the one night everybody dreads, the night before dark week starts\n- AKA finals week- at "f"{college_name}.")
+print("The library was full of "f"{adjective1} students all glued to their books\nand "f"{body_part}" " " "deep in energy drink cans and empty coffee cups.""")
+print("One desperate student even had the guts to sneak in a(n) "f"{an_object}.")
+print("As I expected, I couldn't find a decent place to sit\nso I had to sit next to the dude who smelled like "f"{smell}.")
+print("I began to "f"{action} "f"{adjective2}.")

61ff42d3bb2dbf16486e4ddfc0f3f8119052eeb9
sloaneluckiewicz <77337170+sloaneluckiewicz@users.noreply.github.com>
2021-02-04 10:44:17 -0500

    Delete CSCE204/exercises/feb2 directory

--- CSCE204/exercises/feb2/while.loops1.py
+++
@@ -1,7 +0,0 @@
-userInput = input("Do you want to say hello (Y or N): ") .strip().lower()
-
-while userInput == "y":
-    print("Hello")
-    userInput = input("Do you want to say hello again? ") .lower() .strip()
-
-print("Goodbye")
